471f77e180adff26d8cb83d1563258865f3db9eb
Noah Ma <noahfierz@gmail.com>
2020-04-29 19:23:34 -0700

    last commit didn't have all the files

--- animation-test.rkt
+++ animation-test.rkt
@@ -7,7 +7,7 @@
   "./record.rkt" data/collection
   "./helper.rkt")
 
-(provide shine place-htboxes draw)
+(provide shine place-htboxes draw make-hurt-anim)
 
 (define (shine colour)
   (repeat-for
@@ -17,12 +17,48 @@
                       (rectangle 40 80 "solid" colour))
              40 0
              (rectangle 120 120 "solid" "transparent"))
-    'hurt (list (make-record 'x 40 'y 0 'w 40 'h 80
-                             'freeze 10 'hitstun 20
-                             'pushback '30))
-    'hit (list (make-record 'x 30 'y 0 'w 60 'h 90))
+    'hurt (list (make-record
+                 'x 40 'y 0 'w 40 'h 80))
+    'hit (list (make-record 'x 30 'y 0 'w 60 'h 90
+                            'on-hit (make-record
+                                     'freeze 8 'hitstun 10
+                                     'pushback 15)))
     'speed 3)
    10))
+
+(define (lean-back colour)
+  (make-record
+   'sprite (place-bottom-left
+            (overlay
+             (rotate -90 (triangle 20 "solid" "red"))
+             (polygon (list (make-posn 0 0)
+                            (make-posn 40 0)
+                            (make-posn 50 80)
+                            (make-posn 10 80))
+                    "solid" colour))
+            30 0
+            (rectangle 120 120 "solid" "transparent"))
+   'hurt (list (make-record 'x 30 'y 0 'w 50 'h 80))
+   'hit empty
+   'speed 0))
+
+(define (slide-back d t)  
+  [define first-t (build-sequence t (λ(i) i))]
+  (reverse (map (o round (@ * -1 (/ d (foldl + 0 first-t))))
+                first-t)))
+
+(define/contract (make-hurt-anim hit colour)
+  (-> hash-record? image-color? (sequenceof hash-record?))
+  [define speeds (slide-back (hit 'pushback) (hit 'hitstun))]
+  (build-sequence
+   (+ (hit 'freeze) (hit 'hitstun))
+   (λ(i)
+     (cond
+       [(i . < . (hit 'freeze))
+        (lean-back colour)]
+       [else
+        ((lean-back colour)
+         'speed (nth speeds (- i (hit 'freeze))))]))))
 
 (define (place-htboxes boxes colour background)
   [define (box->image b)
